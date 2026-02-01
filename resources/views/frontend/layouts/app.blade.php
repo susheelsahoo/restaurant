3403eba2f9d91d8f572446f531a2a0092541b4fe
susheelsahoo <susheelcs24@gmail.com>
2026-02-01 14:38:26 +0530

    cotect us page

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -18,6 +18,9 @@ management software, property management systems, and enhance guest experience')
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap"
         rel="stylesheet" />
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
 
     <link href="{{ asset('frontend/css/style.css') }}" rel="stylesheet" />
 </head>
