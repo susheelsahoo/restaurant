aff9e85224e23ba77206660205d01e052cd7097b
susheelsahoo <susheelcs24@gmail.com>
2026-02-02 10:48:39 +0530

    bug fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,14 +10,17 @@ class DashboardController extends Controller
     public function index()
     {
         // addVendors(['amcharts', 'amcharts-maps', 'amcharts-stock']);
-
+        //('new', 'confirmed', 'cancelled', 'complete')	
         $total_bookings = Reservation::count();
         $new_bookings = Reservation::where('status', 'new')->count();
+        $confirmed_bookings = Reservation::where('status', 'confirmed')->count();
+        $cancelled_bookings = Reservation::where('status', 'cancelled')->count();
+        $complete_bookings = Reservation::where('status', 'complete')->count();
 
         // Contact Counts
         $total_contact = ContactMessage::count();
         $new_contact = ContactMessage::where('is_read', false)->count();
 
-        return view('pages.dashboards.index', compact('total_bookings', 'new_bookings', 'total_contact', 'new_contact'));
+        return view('pages.dashboards.index', compact('total_bookings', 'new_bookings', 'confirmed_bookings', 'cancelled_bookings', 'complete_bookings', 'total_contact', 'new_contact'));
     }
 }
