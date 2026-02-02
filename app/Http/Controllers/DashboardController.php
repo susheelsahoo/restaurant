1814270ff011767043731687f33df23ae0c457cb
susheelsahoo <susheelcs24@gmail.com>
2026-02-02 10:43:17 +0530

    bug fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Lead;
+use App\Models\Bookings;
 use App\Models\ContactMessage;
 
 class DashboardController extends Controller
@@ -11,13 +11,13 @@ class DashboardController extends Controller
     {
         // addVendors(['amcharts', 'amcharts-maps', 'amcharts-stock']);
 
-        $total_lead = Lead::count();
-        $new_lead = Lead::where('status', 'new')->count();
+        $total_bookings = Bookings::count();
+        $new_bookings = Bookings::where('status', 'new')->count();
 
         // Contact Counts
         $total_contact = ContactMessage::count();
         $new_contact = ContactMessage::where('is_read', false)->count();
 
-        return view('pages.dashboards.index', compact('total_lead', 'new_lead', 'total_contact', 'new_contact'));
+        return view('pages.dashboards.index', compact('total_bookings', 'new_bookings', 'total_contact', 'new_contact'));
     }
 }
