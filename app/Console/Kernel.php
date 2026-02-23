6f8cff970b5afc9f9a8adf6ee187eb8b281e9135
susheelsahoo <susheelcs24@gmail.com>
2026-02-23 18:12:45 +0000

    sitemap issue and cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,9 +13,13 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
+    protected $commands = [
+        \App\Console\Commands\GenerateSitemap::class,
+    ];
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('generate:sitemap')->weeklyOn(1, '02:00');
+        //$schedule->command('generate:sitemap')->everyMinute();
     }
 
     /**
