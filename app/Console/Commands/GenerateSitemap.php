98d354636912419fdf4b4042e73646f46f59be33
susheelsahoo <susheelcs24@gmail.com>
2026-02-23 22:36:59 +0530

    sitemap

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -2,7 +2,10 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Blog;
+use App\Models\Page;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Schema;
 use Spatie\Sitemap\Sitemap;
 use Spatie\Sitemap\Tags\Url;
 
@@ -13,11 +16,48 @@ class GenerateSitemap extends Command
 
     public function handle()
     {
-        Sitemap::create()
-            ->add(Url::create('/')->setPriority(1.0))
-            ->add(Url::create('/about'))
-            ->add(Url::create('/contact'))
-            ->writeToFile(public_path('sitemap.xml'));
+        $sitemap = Sitemap::create()
+            ->add(Url::create(url('/'))->setPriority(1.0));
+
+        Page::query()
+            ->where('is_active', true)
+            ->whereNotNull('slug')
+            ->where('slug', '!=', '')
+            ->get(['slug', 'updated_at'])
+            ->each(function (Page $page) use ($sitemap) {
+                // Home page is already included as "/"
+                if ($page->slug === 'home') {
+                    return;
+                }
+
+                $sitemap->add(
+                    Url::create(url('/' . ltrim($page->slug, '/')))
+                        ->setLastModificationDate($page->updated_at)
+                );
+            });
+
+        $sitemap->add(Url::create(url('/blogs')));
+
+        $blogQuery = Blog::query()
+            ->whereNotNull('slug')
+            ->where('slug', '!=', '');
+
+        if (Schema::hasColumn('blogs', 'status')) {
+            $blogQuery->where('status', 'published');
+        } elseif (Schema::hasColumn('blogs', 'is_published')) {
+            $blogQuery->where('is_published', true);
+        }
+
+        $blogQuery
+            ->get(['slug', 'updated_at'])
+            ->each(function (Blog $blog) use ($sitemap) {
+                $sitemap->add(
+                    Url::create(url('/blog/' . ltrim($blog->slug, '/')))
+                        ->setLastModificationDate($blog->updated_at)
+                );
+            });
+
+        $sitemap->writeToFile(public_path('sitemap.xml'));
 
         $this->info('Sitemap generated successfully!');
     }
