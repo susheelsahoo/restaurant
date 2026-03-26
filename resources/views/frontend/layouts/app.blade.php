26b50cbd63869ec66c6493c0926bf2b9061a54f7
susheelsahoo <susheelcs24@gmail.com>
2026-03-26 10:43:32 +0530

    update css

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -18,7 +18,7 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap"
         rel="stylesheet" />
-    <link href="{{ asset('frontend/css/style.css') }}" rel="stylesheet" />
+    <link href="{{ asset('frontend/css/style.css') }}?v={{ filemtime(public_path('frontend/css/style.css')) }}" rel="stylesheet" />
 
     <link
         rel="stylesheet"
