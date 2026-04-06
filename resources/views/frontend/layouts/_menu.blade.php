9494be2dc1e75ed400635ddc2e0c5b3522cd0b5b
susheelsahoo <susheelcs24@gmail.com>
2026-04-06 23:45:46 +0530

    book a table isisue

--- resources/views/frontend/layouts/_menu.blade.php
+++ resources/views/frontend/layouts/_menu.blade.php
@@ -65,7 +65,7 @@ $isHome = request()->is('/') || request()->is('home');
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ request()->is('book-a-table') ? 'active' : '' }}" href="/home#reservation">
+                    <a class="nav-link {{ request()->is('book-a-table') ? 'active' : '' }}" href="/#reservation">
                         Book a Table
                     </a>
                 </li>
