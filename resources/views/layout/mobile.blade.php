3f6ab177e13f138cbb0887cfb0dfac51c3fbf5ee
susheelsahoo <susheelcs24@gmail.com>
2026-08-19 23:00:12 +0530

    po update

--- resources/views/layout/mobile.blade.php
+++ resources/views/layout/mobile.blade.php
@@ -37,7 +37,7 @@
         href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
         rel="stylesheet"
     >
-    <link rel="stylesheet" href="{{ asset('mobile-login/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('mobile/style.css') }}">
     @stack('styles')
 </head>
 <body class="@yield('body-class', 'mobile-body')">
